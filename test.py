205f5baca737f332a165932432576209cdf410af
Luiz A F Gomes <luizgomes749@gmail.com>
2019-07-12 16:10:37 -0300

    ;

--- test.py
+++ test.py
@@ -1,28 +1,24 @@
 import asyncio
-import random
-
-my_list = []
-
-def cincoS():
-    print("\n", "5 Segundos depois")
-
-def notify(x):
-    print("VALOR:", x, end="\r")
+import time as tim
 
 
-async def append_task():
+async def notify():
     while True:
-        await asyncio.sleep(5)        
-        cincoS()
+        print(f"Now: {tim.strftime('%X')}         ", end="\r")
+        await asyncio.sleep(1)
 
+async def Ex():
+    while True:
+        print("Visite nosso site...", end="\r")
+        await asyncio.sleep(2)
+        print("www.like.com.br", end="\r")
+        await asyncio.sleep(5)
 
-async def pop_task(a = 0):
-    while True:        
-        notify(a)
-        a += 1
-        await asyncio.sleep(0.01)
+async def main():
+    other = asyncio.create_task(Ex())
+    clock = asyncio.create_task(notify())
 
+    await other
+    await clock
 
-loop = asyncio.get_event_loop()
-cors = asyncio.wait([append_task(), pop_task()])
-loop.run_until_complete(cors)
+asyncio.run(main())
