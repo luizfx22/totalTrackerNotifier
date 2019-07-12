1cc36c2a59c0bea483540c641309696774fc02ae
Luiz A F Gomes <luizgomes749@gmail.com>
2019-07-12 12:24:20 -0300

    Getting TD

--- oTracker.py
+++ oTracker.py
@@ -1,5 +1,6 @@
+from bs4 import BeautifulSoup
 import requests
-import bs4
+
 import json
 
 class TotalNotifyer:
@@ -8,13 +9,15 @@ class TotalNotifyer:
         self.ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
     
     def main(self):
-        self.pagina = requests.get(
-            self.link, headers={'User-Agent': self.ua}
-        )
+        req = requests.get(self.link, headers={'User-Agent': self.ua})
+        req.close()
+
+        bs = BeautifulSoup(req.content, "html.parser")
         
-
-
+        table = bs.find('td', attrs={"width":"51%"})
+        print (table)
 
 
 if __name__ == "__main__":
     l = TotalNotifyer("""https://tracking.totalexpress.com.br/tracking_encomenda.php?code=qeYm9rL%2FzUONCsHcdFBnDZdOAhK6Owza2qbdGHq6lcQ1Nl2k5EASR0PN6BEpqlYc%2BPEmW32Hj8TXD%2B9ZwHMBCmJisKFHg5ruD66XMS8vEmiMVSBpnxY5zvI0fmocxx9cmK7OAbBnT78JRP3WXnP%2FZ%2F6fiLVfwgE6voYTvfzgy0dCLK3%2F""")
+    l.main()
