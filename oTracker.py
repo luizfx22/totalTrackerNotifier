7ae092b627f2789d3309342b47e3534b4b14640f
Luiz A F Gomes <luizgomes749@gmail.com>
2019-07-12 14:08:33 -0300

    Formatted output

--- oTracker.py
+++ oTracker.py
@@ -1,22 +1,46 @@
-from bs4 import BeautifulSoup
+import re
 import requests
-
-import json
+from bs4 import BeautifulSoup
 
 class TotalNotifyer:
     def __init__(self, link):
         self.link = link
         self.ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
-    
+
     def main(self):
+        date = []
+        time = []
+        status = []
+       
         req = requests.get(self.link, headers={'User-Agent': self.ua})
         req.close()
 
         bs = BeautifulSoup(req.content, "html.parser")
+        bs2 = BeautifulSoup(req.content, "html.parser")
         
-        table = bs.find('td', attrs={"width":"51%"})
-        print (table)
+        table = bs.findAll('td', attrs={"width":"51%"})
+        table2 = bs2.findAll('td', attrs={"width": "25%"})
+        table3 = bs2.findAll('td', attrs={"width": "24%"})
 
+        date.clear()
+        time.clear()
+        status.clear()
+
+        for tag in table2:
+            txt = tag.font.text
+            date.append(txt.strip())
+
+        for tag in table3:
+            txt = tag.font.text
+            time.append(txt.strip())
+
+        for tag in table:
+            txt = tag.font.text
+            status.append(txt.strip())        
+        
+        for i in range(len(status)):
+            print (date[i], time[i], status[i])
+        
 
 if __name__ == "__main__":
     l = TotalNotifyer("""https://tracking.totalexpress.com.br/tracking_encomenda.php?code=qeYm9rL%2FzUONCsHcdFBnDZdOAhK6Owza2qbdGHq6lcQ1Nl2k5EASR0PN6BEpqlYc%2BPEmW32Hj8TXD%2B9ZwHMBCmJisKFHg5ruD66XMS8vEmiMVSBpnxY5zvI0fmocxx9cmK7OAbBnT78JRP3WXnP%2FZ%2F6fiLVfwgE6voYTvfzgy0dCLK3%2F""")
