718be0c6f0b8310bb0d0ce357a17cbd1cda932d7
Luiz A F Gomes <luizgomes749@gmail.com>
2019-07-12 14:52:43 -0300

    .

--- oTracker.py
+++ oTracker.py
@@ -6,11 +6,12 @@ class TotalNotifyer:
     def __init__(self, link):
         self.link = link
         self.ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+        self.date = []
+        self.time = []
+        self.status = []
 
     def main(self):
-        date = []
-        time = []
-        status = []
+        
        
         req = requests.get(self.link, headers={'User-Agent': self.ua})
         req.close()
@@ -22,26 +23,29 @@ class TotalNotifyer:
         table2 = bs2.findAll('td', attrs={"width": "25%"})
         table3 = bs2.findAll('td', attrs={"width": "24%"})
 
-        date.clear()
-        time.clear()
-        status.clear()
+        self.date.clear()
+        self.time.clear()
+        self.status.clear()
 
         for tag in table2:
             txt = tag.font.text
-            date.append(txt.strip())
+            self.date.append(txt.strip())
 
         for tag in table3:
             txt = tag.font.text
-            time.append(txt.strip())
+            self.time.append(txt.strip())
 
         for tag in table:
             txt = tag.font.text
-            status.append(txt.strip())        
+            self.status.append(txt.strip())        
         
-        for i in range(len(status)):
-            print (date[i], time[i], status[i])
+        for i in range(len(self.status)):
+            print (self.date[i], self.time[i], self.status[i])
         
 
 if __name__ == "__main__":
-    l = TotalNotifyer("""https://tracking.totalexpress.com.br/tracking_encomenda.php?code=qeYm9rL%2FzUONCsHcdFBnDZdOAhK6Owza2qbdGHq6lcQ1Nl2k5EASR0PN6BEpqlYc%2BPEmW32Hj8TXD%2B9ZwHMBCmJisKFHg5ruD66XMS8vEmiMVSBpnxY5zvI0fmocxx9cmK7OAbBnT78JRP3WXnP%2FZ%2F6fiLVfwgE6voYTvfzgy0dCLK3%2F""")
+    zero = """https://tracking.totalexpress.com.br/tracking_encomenda.php?code=ft6gS%2FErXNjfyhSYu0AYgzXnW5UUb0q%2B6WRTi%2B%2FZZz3KJzRnbZvVQb8EhTWXqIiUmxMvNQpeJ%2B8NUiDwxoi7bYcJsa2FsYWWtN%2FuM3u5%2BdHy1OXQuU77tzHRUsXi8ElOzxXMuGxDcoy3C2jUiFQYgKiDOYVShbsbkBGrG4SZDMy%2FGfpz"""
+    mine = """https://tracking.totalexpress.com.br/tracking_encomenda.php?code=qeYm9rL%2FzUONCsHcdFBnDZdOAhK6Owza2qbdGHq6lcQ1Nl2k5EASR0PN6BEpqlYc%2BPEmW32Hj8TXD%2B9ZwHMBCmJisKFHg5ruD66XMS8vEmiMVSBpnxY5zvI0fmocxx9cmK7OAbBnT78JRP3WXnP%2FZ%2F6fiLVfwgE6voYTvfzgy0dCLK3%2F"""
+
+    l = TotalNotifyer(mine)
     l.main()
